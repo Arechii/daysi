032743f857f0df32f1a9baa7f0b6954914a29f41
Arechi <aglimbenli@gmail.com>
2023-06-18 13:36:23 +0200

    Add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
+import { Analytics } from "@vercel/analytics/react";
 import { type AppType } from "next/app";
-import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import { api } from "~/utils/api";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
