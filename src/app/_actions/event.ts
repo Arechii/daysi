70bc2489f821344fff1e71e33e9b612c3ecf1781
Arechi <aglimbenli@gmail.com>
2023-07-01 16:29:08 +0200

    Add event actions

---
+++ src/app/_actions/event.ts
@@ -0,0 +1,34 @@
+"use server"
+
+import { revalidatePath } from "next/cache"
+import { currentUser } from "@clerk/nextjs"
+import { db } from "~/db"
+import { events } from "~/db/schema"
+import { createId } from "~/utils"
+import { eq } from "drizzle-orm"
+
+export async function getEventsAction() {
+  const user = await currentUser()
+
+  if (!user) return []
+
+  const userEvents = await db.query.events.findMany({
+    where: eq(events.userId, user.id),
+  })
+
+  return userEvents
+}
+
+export async function createEventAction(description: string) {
+  const user = await currentUser()
+
+  if (!user) return
+
+  await db.insert(events).values({
+    id: createId(),
+    userId: user.id,
+    description,
+  })
+
+  revalidatePath("/dashboard")
+}
