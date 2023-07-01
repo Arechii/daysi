246e3113bb7c3020ca2501adbd7a25e6dab94257
Arechi <aglimbenli@gmail.com>
2023-07-01 22:06:53 +0200

    Add dashboard button when already logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,34 @@
-import { SignInButton } from "@clerk/nextjs"
-import { LogInIcon } from "lucide-react"
+import Link from "next/link"
+import { currentUser, SignInButton } from "@clerk/nextjs"
+import { LayoutDashboardIcon, LogInIcon } from "lucide-react"
 
 import { Button } from "~/components/ui/button"
 import TypeAnim from "~/components/type-anim"
 
 export const runtime = "edge"
 
-export default function Home() {
+export default async function Home() {
+  const user = await currentUser()
+
   return (
     <main className="flex h-full flex-col items-center justify-center">
       <div className="container flex flex-col items-center justify-center gap-8 px-4 py-16 ">
         <h2 className="w-full text-center text-xl tracking-wide md:text-3xl">
           Remember the days since you <br /> <TypeAnim />
         </h2>
-        <SignInButton afterSignInUrl="/dashboard" afterSignUpUrl="/dashboard">
-          <Button variant="outline">
-            <LogInIcon className="mr-2 h-4 w-4" /> Sign up
+        {!user ? (
+          <SignInButton afterSignInUrl="/dashboard" afterSignUpUrl="/dashboard">
+            <Button variant="outline">
+              <LogInIcon className="mr-2 h-4 w-4" /> Sign up
+            </Button>
+          </SignInButton>
+        ) : (
+          <Button variant="outline" asChild>
+            <Link href="/dashboard">
+              <LayoutDashboardIcon className="mr-2 h-4 w-4" /> Dashboard
+            </Link>
           </Button>
-        </SignInButton>
+        )}
       </div>
     </main>
   )
