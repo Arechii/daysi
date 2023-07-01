0789a02f51038e93c503808f2e8c1c8981752ff3
Arechi <aglimbenli@gmail.com>
2023-07-01 13:28:06 +0200

    Merge utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,10 @@
 import { clsx, type ClassValue } from "clsx";
+import { customAlphabet } from "nanoid";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
+export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
+
+export const createId = customAlphabet(
+  "0123456789abcdefghijklmnopqrstuvwxyz",
+  10
+);
--- src/utils/id.ts
+++
@@ -1,6 +0,0 @@
-import { customAlphabet } from "nanoid";
-
-export const createId = customAlphabet(
-  "0123456789abcdefghijklmnopqrstuvwxyz",
-  10
-);
