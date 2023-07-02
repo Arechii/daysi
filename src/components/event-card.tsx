b6a74474a120c3e0c0f37103cc529b89b2e68ab1
Arechi <aglimbenli@gmail.com>
2023-07-02 02:19:27 +0200

    Restyle event card

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -2,7 +2,9 @@
 
 import { useEffect, useState } from "react"
 import { type Event } from "~/db/schema"
+import { TimerResetIcon } from "lucide-react"
 
+import { Button } from "./ui/button"
 import { Card, CardContent } from "./ui/card"
 
 const EventCard = ({ description, resetAt, createdAt }: Event) => {
@@ -16,19 +18,22 @@ const EventCard = ({ description, resetAt, createdAt }: Event) => {
 
   return (
     <Card>
-      <CardContent className="flex flex-row items-center justify-between gap-8 pt-6">
-        <div className="flex flex-col">
-          <p>{description}</p>
-          <p className="text-sm text-gray-500 dark:text-gray-400">
-            started on {startedOn}
-          </p>
-        </div>
+      <CardContent className="flex flex-row items-center gap-8 pt-6">
         <p className="text-4xl font-bold text-rose-400">
           {Math.floor(
             Math.abs(resetAt.getTime() - new Date().getTime()) /
               (1000 * 3600 * 24)
           )}
         </p>
+        <div className="flex flex-col">
+          <p className="text-xl">{description}</p>
+          <p className="text-sm text-gray-500 dark:text-gray-400">
+            started on {startedOn}
+          </p>
+        </div>
+        <Button className="ml-auto" variant="outline" size="icon">
+          <TimerResetIcon />
+        </Button>
       </CardContent>
     </Card>
   )
