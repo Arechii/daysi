650cd5cff9a66984de231cf93a05c8fa57e58d4b
Arechi <aglimbenli@gmail.com>
2023-07-02 02:19:05 +0200

    Restyle event list

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,3 @@
-import { ScrollArea } from "~/components/ui/scroll-area"
 import CreateEvent from "~/components/create-event"
 import EventCard from "~/components/event-card"
 
@@ -14,15 +13,11 @@ export default async function Dashboard() {
       <h1 className="decoration-slice text-2xl font-semibold underline decoration-rose-400">
         days since...
       </h1>
-      <ScrollArea className="w-full flex-auto p-2">
-        <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {events.map((e, i) => (
-            <EventCard key={i} {...e} />
-          ))}
-        </div>
-      </ScrollArea>
-      <div className="flex w-full justify-center">
+      <div className="flex flex-col gap-4 p-2">
         <CreateEvent />
+        {events.map((e, i) => (
+          <EventCard key={i} {...e} />
+        ))}
       </div>
     </div>
   )
