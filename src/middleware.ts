2af74c6fd08fd4d3531191b047bc29a58c54e3cd
Arechi <aglimbenli@gmail.com>
2023-07-07 12:07:04 +0200

    Test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,13 @@
+import { NextResponse } from "next/server"
 import { authMiddleware } from "@clerk/nextjs"
 
 export default authMiddleware({
   publicRoutes: ["/"],
+  afterAuth(auth) {
+    if (auth.isPublicRoute) {
+      return NextResponse.next()
+    }
+  },
 })
 
 export const config = {
