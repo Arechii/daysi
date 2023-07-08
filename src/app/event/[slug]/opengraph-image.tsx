82bfff2f8952871d64d9397ba1a8b22d3607d0e5
Arechi <aglimbenli@gmail.com>
2023-07-09 00:20:27 +0200

    Update event og image

--- src/app/event/[slug]/opengraph-image.tsx
+++ src/app/event/[slug]/opengraph-image.tsx
@@ -1,4 +1,7 @@
+/* eslint-disable @next/next/no-img-element */
+import { notFound } from "next/navigation"
 import { ImageResponse } from "next/server"
+import { clerkClient } from "@clerk/nextjs"
 import { db } from "~/db"
 import { events } from "~/db/schema"
 import { eq } from "drizzle-orm"
@@ -15,16 +18,40 @@ export default async function Image({ params }: { params: { slug: string } }) {
     where: eq(events.id, params.slug),
   })
 
+  if (!event) {
+    return notFound()
+  }
+
+  const user = await clerkClient.users.getUser(event.userId)
+  const daysSince = Math.floor(
+    Math.abs(event.resetAt.getTime() - new Date().getTime()) /
+      (1000 * 3600 * 24)
+  )
+
   return new ImageResponse(
     (
-      <div tw="flex h-full w-full items-center justify-center text-5xl gap-4">
-        🌺
-        {event?.description}
+      <div tw="flex h-full w-full">
+        <div tw="flex h-full w-full flex-col items-center justify-around rounded-xl border-2 border-rose-400 bg-white">
+          <div tw="flex w-full items-center justify-center font-extrabold tracking-wide">
+            <div tw="mr-4 text-6xl text-rose-400">{daysSince}</div>
+            <div tw="text-6xl text-zinc-700">days since</div>
+          </div>
+          <div tw="flex text-center text-6xl tracking-wide text-rose-400">
+            {event?.description}
+          </div>
+          <div tw="flex items-center">
+            <img
+              tw="mr-4 h-14 w-14 rounded-full"
+              src={user.profileImageUrl}
+              alt={user.username ?? ""}
+            />
+            <span tw="text-4xl text-zinc-700">{user.username}</span>
+          </div>
+        </div>
       </div>
     ),
     {
       ...size,
-      emoji: "twemoji",
     }
   )
 }
