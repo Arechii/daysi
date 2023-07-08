7693cf5bafaf5bd687789bc6bdbd80d0d4ab2c1f
Arechi <aglimbenli@gmail.com>
2023-07-08 17:03:05 +0200

    Update layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+import { CalendarHeartIcon } from "lucide-react"
+
 import CreateEvent from "~/components/create-event"
 import Events from "~/components/events"
 
@@ -10,8 +12,8 @@ export default async function Dashboard() {
 
   return (
     <div className="container -mt-16 flex h-full flex-col gap-8 px-4 py-16">
-      <h1 className="decoration-slice text-2xl font-semibold underline decoration-rose-400">
-        days since...
+      <h1 className="flex items-center gap-1 text-2xl font-semibold underline decoration-rose-400">
+        <CalendarHeartIcon /> Events
       </h1>
       <div className="flex flex-col gap-4 p-2">
         <CreateEvent />
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 import { SignInButton } from "@clerk/nextjs"
 import { type User } from "@clerk/nextjs/dist/types/server"
-import { LogInIcon } from "lucide-react"
+import { FlowerIcon, LogInIcon } from "lucide-react"
 
 import ThemeToggle from "./theme-toggle"
 import { Button } from "./ui/button"
@@ -14,8 +14,8 @@ const Navbar = ({
   return (
     <div className="sticky left-0 top-0 flex w-screen justify-between p-4">
       <Link href="/">
-        <h1 className="text-2xl font-extrabold tracking-tight text-rose-400">
-          daysi
+        <h1 className="flex items-center gap-1 text-2xl font-bold tracking-wide text-rose-400">
+          <FlowerIcon /> Daysi
         </h1>
       </Link>
       <div className="flex flex-row-reverse gap-2">
