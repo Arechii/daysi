f08fca4626e51fafbead93ac5b52ba46d2faf9af
Arechi <aglimbenli@gmail.com>
2023-07-21 12:10:14 +0200

    Add reset model

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { type InferModel } from "drizzle-orm"
+import { relations, type InferModel } from "drizzle-orm"
 import {
   index,
   mysqlTableCreator,
@@ -17,7 +17,6 @@ export const events = mysqlTable(
     userId: varchar("userId", { length: 32 }).notNull(),
     description: varchar("description", { length: 128 }).notNull(),
     startedAt: timestamp("startedAt").defaultNow().notNull(),
-    resetAt: timestamp("resetAt").defaultNow().notNull(),
     createdAt: timestamp("createdAt").defaultNow(),
     updatedAt: timestamp("updatedAt").defaultNow().onUpdateNow(),
   },
@@ -26,10 +25,37 @@ export const events = mysqlTable(
   }),
 )
 
-export type Event = InferModel<typeof events>
+export const resets = mysqlTable(
+  "resets",
+  {
+    id: varchar("id", { length: 10 }).primaryKey(),
+    eventId: varchar("eventId", { length: 10 }).notNull(),
+    createdAt: timestamp("createdAt").defaultNow(),
+    updatedAt: timestamp("updatedAt").defaultNow().onUpdateNow(),
+  },
+  (resets) => ({
+    eventIdIndex: index("eventid_idx").on(resets.eventId),
+  }),
+)
+
+export const eventsRelations = relations(events, ({ many }) => ({
+  resets: many(resets),
+}))
+
+export const resetsRelations = relations(resets, ({ one }) => ({
+  event: one(events, { fields: [resets.eventId], references: [events.id] }),
+}))
+
+export type Event = InferModel<typeof events> & {
+  resets: Omit<Reset, "event">[]
+}
+export type Reset = InferModel<typeof resets> & { event: Omit<Event, "resets"> }
 
 export const selectEventSchema = createSelectSchema(events)
 export const insertEventSchema = createInsertSchema(events, {
   description: (schema) => schema.description.min(3),
   startedAt: z.date(),
 })
+
+export const selectResetSchema = createSelectSchema(resets)
+export const insertResetSchema = createInsertSchema(resets)
