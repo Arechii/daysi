787800fcd43906344f4711df48c409eadc0ba237
Arechi <aglimbenli@gmail.com>
2023-08-06 13:24:52 +0200

    Suppress html hydration warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={twMerge(
           "flex h-screen flex-col gap-2 font-sans",
